1d8799fd31e2dcb0bfb14819283d4f8f452c8e0a
PSScript <deepsands@live.de>
2025-11-14 09:28:49 +0100

    Create Forward-Export_AD.ps1

---
+++ Forward-Export_AD.ps1
@@ -0,0 +1,35 @@
+﻿Get-ADUser -Filter { altRecipient -like "*" } `
+    -Properties altRecipient, deliverAndRedirect, mail, proxyAddresses |
+    ForEach-Object {
+
+        #
+        # Resolve FORWARDING TARGET (altRecipient)
+        #
+        $target = Get-ADObject $_.altRecipient -Properties mail, proxyAddresses
+
+        $targetSmtp = $target.mail
+        if (-not $targetSmtp -and $target.proxyAddresses) {
+            $targetSmtp = ($target.proxyAddresses |
+                           Where-Object { $_ -like "SMTP:*" } |
+                           ForEach-Object { $_.Substring(5) }) |
+                           Select-Object -First 1
+        }
+
+        #
+        # Resolve USER'S OWN SMTP
+        #
+        $userSmtp = $_.mail
+        if (-not $userSmtp -and $_.proxyAddresses) {
+            $userSmtp = ($_.proxyAddresses |
+                         Where-Object { $_ -like "SMTP:*" } |
+                         ForEach-Object { $_.Substring(5) }) |
+                         Select-Object -First 1
+        }
+
+        [PSCustomObject]@{
+            UserSMTP           = $userSmtp
+            DisplayName        = $_.DisplayName
+            ForwardToSMTP      = $targetSmtp
+            DeliverAndKeepCopy = $_.deliverAndRedirect
+        }
+    } | Export-Csv "c:\temp\ForwardingResolved.csv" -NoTypeInformation -Encoding UTF8
